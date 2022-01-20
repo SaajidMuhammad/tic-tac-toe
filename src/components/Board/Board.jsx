4a6c618c62e127252d372f2f2eda6fe75984f8e6
Saajid Muhammad <saajithtm@gmail.com>
2022-01-20 14:57:53 +0530

    Basic Structure

---
+++ src/components/Board/Board.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import "./Board.css"
+
+import Box from '../Box/Box'
+
+const boxId = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+
+export default function Board() {
+
+  return <div className='boardWrapper'>
+
+    {boxId.map((bId, i) => {
+      return (
+        <Box />
+      )
+
+    })}
+
+
+
+  </div>;
+}
